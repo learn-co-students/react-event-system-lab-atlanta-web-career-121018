d6d9333346a226004b50af6bc2b4d0c3afc53c33
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-21 15:59:32 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+   constructor() {
+      super()
+   }  
+
+   render () {
+      return (
+         <button type="password" onFocus={()=>{console.log('Good!')}} onBlur={()=> {console.log('Hey! Eyes on me!')}} />
+      )
+   }
+}
